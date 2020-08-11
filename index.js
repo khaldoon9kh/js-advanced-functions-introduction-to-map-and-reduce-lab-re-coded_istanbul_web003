9eb1ee4e87f7de84efc175252a3483312a078ff4
khaldoon khaldi <khaldoon9kh9@gmail.com>
2020-08-11 17:49:56 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+const a = [1,2,3,-9];
+function mapToNegativize (x) {
+    return x.map (x => x * -1)
+};
+
+function mapToNoChange (y) {
+    return y.map (y => y)
+};
+
+
+function mapToDouble (x) {
+    return x.map (x => x * 2)
+};
+
+function mapToSquare (x) {
+    return x.map (x => x * x);
+};
+
+
+function reduceToTotal(s,t) {
+    if  (t === undefined) {
+        return s.reduce ((curr,accu) => curr + accu)
+    }
+    return s.reduce ((curr,accu) => curr + accu,t)
+};
+
+function checktrue (x){
+    if (Boolean(x) === true){
+        return true;
+    }
+    return false;
+};
+function reduceToAllTrue(t){
+    return t.reduce(checktrue);
+};
